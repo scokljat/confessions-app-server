82cd16648fe99e80a00af5faa9a9307699996adb
scokljat <90629541+scokljat@users.noreply.github.com>
2022-02-01 19:12:09 +0100

    Adds timestamps

--- models/postMessage.js
+++ models/postMessage.js
@@ -1,21 +1,20 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema({
-  description: String,
-  creator: String,
-  likeCount: {
-    type: Number,
-    default: 0,
+const postSchema = mongoose.Schema(
+  {
+    description: String,
+    creator: String,
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+    unlikeCount: {
+      type: Number,
+      default: 0,
+    },
   },
-  unlikeCount: {
-    type: Number,
-    default: 0,
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-});
+  { timestamps: true }
+);
 const PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
