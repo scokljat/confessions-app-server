bdce9e7fc44f41e01a1e6491e4992f8c6b8b2224
scokljat <90629541+scokljat@users.noreply.github.com>
2022-02-02 20:41:06 +0100

    Adds body-parser

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv/config");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const postRoutes = require("./routes/posts");
 
 const app = express();
 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
